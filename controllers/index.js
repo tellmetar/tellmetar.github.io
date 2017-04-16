5575a476d0ee80a9d3911a55932ebed1c8c263a7
tellmetar <evantellme@gmail.com>
2017-04-16 17:37:32 +0800

    ,

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 var fn_index = async (ctx, next) => {
-    ctx.response.body = ../signin.html;
+    ctx.response.body = ./signin.html;
 };
 
 var fn_signin = async (ctx, next) => {
@@ -7,8 +7,8 @@ var fn_signin = async (ctx, next) => {
         name = ctx.request.body.name || '',
         password = ctx.request.body.password || '';
     console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = index.html;
+    if (name === 'haha@haha.com' && password === '123456') {
+        ctx.response.body = ./index.html;
     } else {
         ctx.response.body = `<h1>Login failed!</h1>
         <p><a href="/">Try again</a></p>`;
